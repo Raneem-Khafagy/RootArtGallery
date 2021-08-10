8200d8de4f20d2c3756a09efc6fd31933e4605ca
Raneem Khafagy <69002626+Raneem-Khafagy@users.noreply.github.com>
2021-08-10 18:57:39 +0200

    added carousel

---
+++ resources/views/components/carousel.blade.php
@@ -0,0 +1,48 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img class="first-slide" src="{{$first_slide}}" class="img-responsive"
+                alt="First slide">
+            <div class="container">
+                <div class="carousel-caption text-left">
+                    <h1>Art headline.</h1>
+                    <p class="d-none d-md-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
+                        elit non mi porta gravida
+                        at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-dark" href="#"role="button">Contact us</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="second-slide" src="{{$second_slide}}" class="img-responsive" alt="Second slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1>Another Art headline.</h1>
+                    <p class="d-none d-md-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
+                        elit non mi porta gravida
+                        at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-dark" href="#" role="button">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="third-slide" src="{{$third_slide}}" class="img-responsive"
+                alt="Third slide">
+            <div class="container">
+                <div class="carousel-caption text-right">
+                    <h1>One more for good measure.</h1>
+                    <p class="d-none d-md-block">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id
+                        elit non mi porta gravida
+                        at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-dark" href="#" role="button">Browse gallery</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,19 @@
 active
 @endsection
 @section('content')
-<img src="{{asset('storage/rev-image-1.jpg')}}">
+{{-- Carousel --}}
+@component('components.carousel')
+@slot('first_slide')
+{{asset('storage/rev-home-image-1.jpg')}}
+@endslot
+@slot('second_slide')
+{{asset('storage/rev-home-image-2.jpg')}}
+@endslot
+@slot('third_slide')
+{{asset('storage/rev-home-image-3.jpg')}}
+@endslot
+@endcomponent
+{{-- end carousel --}}
 <h1>ghkasdcvashcvasvcghas</h1>
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae sint ipsa sequi inventore quas aperiam culpa odio, modi eius dolore veritatis ratione facilis dicta fuga eos ullam iure distinctio magni.lorem </p>
 @endsection
