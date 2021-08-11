be170043c71da667821d08bbaf9929ea9aa5bd03
Raneem Khafagy <69002626+Raneem-Khafagy@users.noreply.github.com>
2021-08-11 16:08:08 +0200

    added carousel to about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,3 +2,24 @@
 @section('headers')
 <title>about</title>
 @endsection
+@section('active_about')
+active
+@endsection
+@section('content')
+{{-- Carousel --}}
+<div id="about-text">
+@component('components.carousel')
+@slot('first_slide')
+{{asset('storage/about-rev-image-2.jpg')}}
+@endslot
+@slot('second_slide')
+{{asset('storage/about-rev-image-1.jpg')}}
+@endslot
+@slot('third_slide')
+{{asset('storage/rev-home-image-3.jpg')}}
+@endslot
+@endcomponent
+</div>
+{{-- end carousel --}}
+
+@endsection
